14b1d6222f1c39c5b4d66e0da8dd5967fd4bfb5c
erikvullings <erik.vullings@gmail.com>
2011-02-28 13:43:17 +0100

    Added missing files

---
+++ XmlEditor.Presentation/Views/XmlView.xaml.cs
@@ -0,0 +1,23 @@
+﻿#region
+
+using System.ComponentModel.Composition;
+using XmlEditor.Applications.Views;
+
+#endregion
+
+namespace XmlEditor.Presentation.Views
+{
+    [Export(typeof (IXmlView)), PartCreationPolicy(CreationPolicy.NonShared)]
+    public partial class XmlView : IXmlView
+    {
+        public XmlView() {
+            InitializeComponent();
+        }
+
+        //private ScenarioViewModel ViewModel { get { return DataContext as ScenarioViewModel;}}
+
+        //private void TabControl_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e) {
+        //    if (e.)
+        //}
+    }
+}
